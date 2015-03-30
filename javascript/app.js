0d4bf6b9d730c2cfbd73c07b44d00fb6bb59ac79
Kelsey Ando <kma3a@virginia.edu>
2015-03-30 17:39:47 -0400

    adding the config file

--- javascript/app.js
+++ javascript/app.js
@@ -1,1 +1,9 @@
 var myApp = angular.module("myApp", ["ui-router"]);
+myApp.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state("userslist", {
+      url: '/users',
+      controller: "UserContrller as users",
+      templateUrl: '../partials/user_index.html'
+   });
+})
