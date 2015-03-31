09963bb7a5a378e795d2d71e387fdf79697f0ce3
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2015-03-31 11:13:40 -0400

    Fixed some collisions between the controllers

--- javascript/newMew.controller.js
+++ javascript/newMew.controller.js
@@ -1,4 +1,4 @@
-angular.module('myApp.controllers', [])
+angular.module('myApp.controllers')
 	.controller("MewController", function($scope){
   		$scope.message = "Make a Mew";
   		$scope.mewComment = "What are you mewing about?!";
