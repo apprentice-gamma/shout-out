a5470f1e5345d96a0363cfa78c0b749c9754d78e
Kelsey Ando <kma3a@virginia.edu>
2015-03-31 09:43:33 -0400

    removed a module because it was not needed

--- javascript/app.js
+++ javascript/app.js
@@ -1,4 +1,4 @@
-var myApp = angular.module("myApp", ["ngRoute","ngResource"]);
+var myApp = angular.module("myApp", ["ngRoute"]);
 myApp.config(['$routeProvider', function($routeProvider) {
   $routeProvider.when("/", {
       controller: "MainController",
