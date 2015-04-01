704fe0aadda97a03cc413fb4e30fa2bcb9a6c010
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2015-04-01 15:49:55 -0400

    Had to delete the branch since it was out of date and re-do it. All the validator does right now is hide the submit button until text is entered.

--- javascript/newMew.controller.js
+++ javascript/newMew.controller.js
@@ -1,5 +1,13 @@
 angular.module('myApp.controllers')
 	.controller("MewController", function($scope){
+		$scope.validate = function($scope.mewComment){
+			var mew = mewComment.trim();
+			if (mew != "" && mew != "What are you mewing about?!"){
+				$scope.mewSet=true;
+			}
+		}
+		$scope.mewSet = false;
   		$scope.message = "Make a Mew";
   		$scope.mewComment = "What are you mewing about?!";
+	
 	})
