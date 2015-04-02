f5595515b8b578b698a33cffaea91810e2822f98
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2015-04-02 11:10:05 -0400

    Made it so that the Mew comment needs to be at least one character long.

--- javascript/newMew.controller.js
+++ javascript/newMew.controller.js
@@ -2,8 +2,5 @@ angular.module('myApp.controllers')
 	.controller("MewController", function($scope){
 		var defaultMew = "What are you mewing about?!";
   		$scope.message = "Make a Mew";
-  		$scope.mewComment = defaultMew;
-  		
-
-		};
+  		$scope.mewComment = defaultMew;  		
 	})
