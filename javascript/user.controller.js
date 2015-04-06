dee2372e205de8e17d47a6b1477f4ca9f3c5165b
bitingteresa <teresa@scientificlee.com>
2015-04-06 16:17:03 -0400

    its alive but its a monster

--- javascript/user.controller.js
+++ javascript/user.controller.js
@@ -1,11 +1,9 @@
 angular.module('myApp.controllers')
 	.controller("UserController", function($scope, $http, UserFactory){
-	  	 $http.get('http://project1-backend.herokuapp.com/api/users').then (function ($scope) {
-			$scope.all = result.data;
-		}
-		) 
+	  	$scope.all = UserFactory.query();
 		$scope.go = function ( path ) {
   			$location.path( path );
   		}
 	})
 
+//$scope.oneUser = UserFactory.get({user:userId})
--- javascript/user.factory.js
+++ javascript/user.factory.js
@@ -1,7 +1,6 @@
 angular.module('myApp.factory', [])
-	.factory("UserFactory", function($resource, $http) {
-		return $http.get('https://project1-backend.herokuapp.com/api/users').then (function ($scope) {
-			scope.users = result.data;
-		}
-		) 
+	.factory("UserFactory", function($resource) {
+		return $resource ('https://project1-backend.herokuapp.com/api/users/:user_id', 
+			{user_id: '@user_id'});
+
 	});
