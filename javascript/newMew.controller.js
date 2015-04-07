33a4784ec558870ea12ee683e582e12a10c1ad96
KittyGarrison <[katrina.garrison@detroitlabs.com]>
2015-04-07 16:54:26 -0400

    consol logging the mew text by submit btn

--- javascript/newMew.controller.js
+++ javascript/newMew.controller.js
@@ -1,11 +1,16 @@
 angular.module('myApp.controllers')
 	.controller("MewController", function($scope, $location, UserFactory, $routeParams){
 		$scope.user = UserFactory.get({user_id: $routeParams.id});
-		var defaultMew = "What are you mewing about?!";
-  		$scope.message = "Make a Mew";
-  		$scope.mewComment = defaultMew;  		
+		$scope.shout = {};
+		console.log($scope.user)
+		// var defaultMew = "What are you mewing about?!";
+  		$scope.message = "Give a Mew to ";
+  		// $scope.mewComment = defaultMew;  		
 		$scope.go = function ( path ) {
-  		$location.path( path );
-};
+			$scope.shout = angular.copy($scope.shout);
+			console.log($scope.shout);
+	  		// $location.path( path );
+
+		};
 
 	})
