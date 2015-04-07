512d219c15cb4bcae7d8451bbf0f8faebd81d01e
KittyGarrison <[katrina.garrison@detroitlabs.com]>
2015-04-07 15:19:37 -0400

    no longer jacked, cant enstantiate twice

--- javascript/mew.factory.js
+++ javascript/mew.factory.js
@@ -1,4 +1,4 @@
-angular.module('myApp.factory', [])
+angular.module('myApp.factory')
 	.factory("MewFactory", function($resource) {
 		return $resource ('https://project1-backend.herokuapp.com/api/users/:user_id/shoutouts/', 
 			{user_id: '@user_id'});
