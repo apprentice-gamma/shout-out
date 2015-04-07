666f06536a9689294015c116d62f44e60de18e4e
KittyGarrison <[katrina.garrison@detroitlabs.com]>
2015-04-07 14:52:41 -0400

    everything is jacked

--- javascript/newMew.controller.js
+++ javascript/newMew.controller.js
@@ -1,5 +1,6 @@
 angular.module('myApp.controllers')
-	.controller("MewController", function($scope, $location){
+	.controller("MewController", function($scope, $location, UserFactory, $routeParams){
+		$scope.user = UserFactory.get({user_id: $routeParams.id});
 		var defaultMew = "What are you mewing about?!";
   		$scope.message = "Make a Mew";
   		$scope.mewComment = defaultMew;  		
