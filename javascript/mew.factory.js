b2ac5a7d1c8b5bd838c57908e72ab0032e5c9048
KittyGarrison <[katrina.garrison@detroitlabs.com]>
2015-04-08 14:40:16 -0400

    working together we got the submit form to work

--- javascript/mew.factory.js
+++ javascript/mew.factory.js
@@ -1,6 +1,8 @@
 angular.module('myApp.factory')
 	.factory("MewFactory", function($resource) {
-		return $resource ('https://project1-backend.herokuapp.com/api/users/:user_id/shoutouts', 
-			{user_id: '@user_id'});
+		return $resource (
+			'https://project1-backend.herokuapp.com/api/users/:user_id/shoutouts', 
+			{user_id: '@user_id'}
+		)
 
 	});
--- javascript/newMew.controller.js
+++ javascript/newMew.controller.js
@@ -1,15 +1,13 @@
 angular.module('myApp.controllers')
-	.controller("MewController", function($scope, $location, UserFactory, $routeParams){
+	.controller("MewController", function($scope, $location, UserFactory, $routeParams, MewFactory){
 		$scope.user = UserFactory.get({user_id: $routeParams.id});
-		$scope.shout = {};
-		console.log($scope.user)
-		// var defaultMew = "What are you mewing about?!";
+
   		$scope.message = "Give a Mew to ";
-  		// $scope.mewComment = defaultMew;  		
-		$scope.go = function ( path ) {
-			$scope.shout = angular.copy($scope.shout);
-			console.log($scope.shout);
-	  		// $location.path( path );
+  		$scope.shout = {'text':"What are you mewing about?!"};  		
+		$scope.go = function (path) {
+			$scope.shout.user_id = $routeParams.id;
+			MewFactory.save($scope.shout);
+	  		$location.path( path );
 
 		};
 
