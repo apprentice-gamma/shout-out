85049a02156c2514da7bdd9bfc30025bef66fc9b
Kelsey Ando <kma3a@virginia.edu>
2015-04-09 17:22:08 -0400

    adding timeout so that it will show the mew on refresh

--- javascript/newMew.controller.js
+++ javascript/newMew.controller.js
@@ -1,14 +1,16 @@
 angular.module('myApp.controllers')
-	.controller("MewController", function($scope, $location, UserFactory, $routeParams, MewFactory){
+	.controller("MewController", function($scope, $location, UserFactory, $routeParams, MewFactory, $timeout){
 		$scope.user = UserFactory.get({user_id: $routeParams.id});
 
   		$scope.message = "Give a Mew to ";
-  		$scope.shout = {'text':"What are you mewing about?!"};  		
-		$scope.go = function (path) {
+  		$scope.shout = {'text':"What are you mewing about?!"};
+  		$scope.redirect = function() {
+  			$location.path('/users/' + $routeParams.id)
+  		}  		
+		$scope.go = function () {
 			$scope.shout.user_id = $routeParams.id;
 			MewFactory.save($scope.shout);
-	  		$location.path( path );
-
+	  		$timeout($scope.redirect, 1000);
 		};
 
 	})
